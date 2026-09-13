9d63d76c338278f6e71d0b0a7bf13a1975a4d720
MEANINGLESS <3399134897@qq.com>
2026-09-13 13:24:16 +0800

    Update rumanhua.js

--- normal_comic/rumanhua.js
+++ normal_comic/rumanhua.js
@@ -191,7 +191,7 @@ class RuManhua extends ComicSource {
   minAppVersion = "1.6.0";
 
   // 更新链接，请替换为你自己的托管地址
-  url = "https://cdn.jsdelivr.net/gh/meaninglesslyy/venera-configs@main/mangaforfree.js";
+  url = "";
 
   get baseUrl() {
     return "https://www.rumanhua.org";
