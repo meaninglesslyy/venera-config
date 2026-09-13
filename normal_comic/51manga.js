2aec30a7f5b2d93d6ecfac016bc3d6dbefed7774
MEANINGLESS <3399134897@qq.com>
2026-09-13 13:21:15 +0800

    Update 51manga.js

--- normal_comic/51manga.js
+++ normal_comic/51manga.js
@@ -192,7 +192,7 @@ class Manga51 extends ComicSource {
   minAppVersion = "1.6.0";
 
   // 更新链接，请替换为你自己的托管地址
-  url = "";
+  url = "https://cdn.jsdelivr.net/gh/meaninglesslyy/venera-configs@main/mangaforfree.js";
 
   get baseUrl() {
     return "https://m.51manga.com";
