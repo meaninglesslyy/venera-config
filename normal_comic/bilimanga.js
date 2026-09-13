c740c0ca70caaece3975bb8f2b083f5461f0d147
MEANINGLESS <3399134897@qq.com>
2026-09-13 14:29:04 +0800

    Update bilimanga.js

--- normal_comic/bilimanga.js
+++ normal_comic/bilimanga.js
@@ -24,7 +24,7 @@ class BiliManga extends ComicSource {
   minAppVersion = "1.6.0";
 
   // 更新链接，请替换为你自己的托管地址
-  url = "";
+  url = "https://cdn.jsdelivr.net/gh/meaninglesslyy/venera-config@main/normal_comic/bilimanga.js";
 
   get baseUrl() {
     return "https://www.bilimanga.net";
